2285d997310c5fc78d4dea8001fabe4028365eb4
hansung <hansung080@hanmail.net>
2023-11-27 20:54:09 +0900

    modified a log from source to sourced in init.sh

--- hidden/init.sh
+++ hidden/init.sh
@@ -4,8 +4,8 @@ h_init() {
   local H_VERBOSE= H_SOURCE_ENABLE= H_SOURCE_FORCE=
   local feature
   while IFS= read -rd '' feature; do
-    [[ "$1" == '-v' ]] && echo "source $feature"
     source "$feature"
+    [[ "$1" == '-v' ]] && echo "sourced $feature"
   done < <(find "$H_ANYSH_DIR/hidden" "$H_ANYSH_DIR/features" -type f -name '[^.]*.sh' -print0)
 }
 
