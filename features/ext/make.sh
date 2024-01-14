d31e4a58ac61394ab31b07869b37e3b7cce3ac3c
hansung <hansung080@hanmail.net>
2024-01-14 19:22:11 +0900

    added h_make_help and h_make_usage

--- features/ext/make.sh
+++ features/ext/make.sh
@@ -67,3 +67,44 @@ h_make_update() {
 }
 
 alias makev='make __verbose=true'
+
+h_make_help() {
+  h_echo 'Usage:'
+  h_echo '  make [<options...>] [<targets...>]'
+  h_echo
+  h_echo 'Options:'
+  h_echo '  --helpx        Display this help message'
+  h_echo
+  h_echo 'Usage by Targets:'
+  h_echo '  make new <project>   Create a <project>, default: --bin'
+  h_echo '    --bin              Create a binary project'
+  h_echo '    --lib              Create a library project'
+  h_echo '  make status          Check if local makefiles are up-to-date'
+  h_echo '  make update          Download bin.mk, lib.mk, or project.mk if they exist in the local directory'
+  h_echo '    --bin              Download bin.mk regardless of its existence'
+  h_echo '    --lib              Download lib.mk regardless of its existence'
+  h_echo '    --project          Download project.mk regardless of its existence'
+  h_echo '    --all              Download bin.mk, lib.mk, and project.mk regardless of their existence'
+  h_echo '  make [build]         Build the project, [build] can be omitted'
+  h_echo '    -v, --verbose      Display stdout for build, default: no stdout'
+  h_echo "                       CAUTION: -v is also used by command make. Thus, 'make --version' can be used for 'command make -v'."
+  h_echo '    --static           Build a static library for a library project, default: dynamic library'
+  h_echo '  make run             Build and run the project'
+  h_echo '    --args <args>      Pass <args> to the program'
+  h_echo '  make test            Build and test the project'
+  h_echo '    --args <args>      Pass <args> to the test-program'
+  h_echo '  make clean           Clean the project'
+  h_echo '  make rename-project  Rename the project in test code'
+  h_echo '    --old-project <name>  Specify the old project name'
+  h_echo '    --new-project <name>  Specify the new project name'
+  h_echo '  make version         Display the version of the makefile'
+  h_echo '  make var             Display variables defined in the makefile (for debugging)'
+  h_echo '  make env             Display environment variables used in the makefile (for debugging)'
+  h_echo '  make all             Build all projects including dependent projects'
+  h_echo '  make clean-all       Clean all projects including dependent projects'
+  h_echo '  make ext             Display extended variables defined in project.mk (for debugging)'
+}
+
+h_make_usage() {
+  h_error "Run 'make --helpx' for more information on the usage."
+}
